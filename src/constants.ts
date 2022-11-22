57e4e427c2e65fe5bcc8f57b4de65a8d00f27338
vvmakarov <vvmakarov@corp.finam.ru>
2022-11-22 21:08:19 +0300

    2022-11-22--11

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,10 @@ export const MILLIS_IN_DAY = 86_400_000;
 export const MILLIS_IN_HOUR = 3_600_000;
 
 const DEBUG = (String(process.env.DEBUG || '')).trim();
-export const DEBUG_SQL = /\baf-streams:sql\b/i.test(DEBUG); // Portion SQL
-export const DEBUG_LTR = /\baf-streams:ltr\b/i.test(DEBUG); // LastTimeRecords
-export const DEBUG_LNP = /\baf-streams:lnp\b/i.test(DEBUG); // before & after load next portion
-export const DEBUG_STREAM = /\baf-streams:stream\b/i.test(DEBUG);
+const isTotalDebug = DEBUG === '*';
+const isTotalStreamDebug = isTotalDebug || /\baf-streams:?\*/i.test(DEBUG);
+
+export const DEBUG_SQL = isTotalStreamDebug || /\baf-streams:sql\b/i.test(DEBUG); // Portion SQL
+export const DEBUG_LTR = isTotalStreamDebug || /\baf-streams:ltr\b/i.test(DEBUG); // LastTimeRecords
+export const DEBUG_LNP = isTotalStreamDebug || /\baf-streams:lnp\b/i.test(DEBUG); // before & after load next portion
+export const DEBUG_STREAM = isTotalStreamDebug || /\baf-streams:stream\b/i.test(DEBUG);
